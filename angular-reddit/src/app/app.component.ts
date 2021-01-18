c21fca307f8b31a1a26f105b7b118abb4e66dfea
Siriani1 <siriani.andrea@iisgalvanimi.edu.it>
2021-01-18 09:38:16 +0000

    finito

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    let t = title.value;
+    let v= link.value;
+    console.log(`Adding article title: `+t+ ` and link: `+ v);
+    console.log(`Adding article title: `+title.value+ ` and link: `+ link.value);
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
   return false;
   }
