3b44ee56a011642fc67ef052009928feb8b1784c
Siriani1 <siriani.andrea@iisgalvanimi.edu.it>
2021-01-18 09:24:25 +0000

    quasi finito

---
+++ angular-spam/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  articles = new Array<string>();
+
+  spam(spamText: HTMLInputElement, num: HTMLInputElement): boolean {
+    let cont = Number(num.value);
+    console.log(`spamText: ${spamText.value} and spamNum: ${num.value}`);
+    for(let i:number =0; i< cont; i++){
+
+      this.articles.push(i + " " + spamText.value)
+
+    }
+    return false;
+  }
+
+}
---
+++ angular-spam/src/app/spam/spam.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-spam',
+  templateUrl: './spam.component.html',
+  styleUrls: ['./spam.component.css']
+})
+export class SpamComponent implements OnInit {
+  @Input() article: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
