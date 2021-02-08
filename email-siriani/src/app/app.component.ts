35eb26111308786ae0b919b357e382318de06ba1
Siriani1 <siriani.andrea@iisgalvanimi.edu.it>
2021-02-08 09:26:57 +0000

    quarto punto

--- email-siriani/src/app/app.component.ts
+++ email-siriani/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Email } from './models/email.model';
+import { TestObject } from 'protractor/built/driverProviders';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'email-siriani';
+  emails:Email[];
+
+  constructor(){
+    this.emails = []
+
+  }
+
+  sendEmail(a:HTMLInputElement, oggetto:HTMLInputElement, testo:HTMLInputElement): boolean{
+    this.emails.push(new Email(a.value,oggetto.value,testo.value))
+    a.value = '';
+    oggetto.value = '';
+    testo.value = '';
+    return false;
+  }
 }
---
+++ email-siriani/src/app/models/email.model.ts
@@ -0,0 +1,24 @@
+export class Email{
+  email: string;
+  oggetto: string;
+  testo: string;
+
+
+  constructor(email: string, oggetto: string, testo: string) {
+    this.email = email;
+    this.oggetto = oggetto;
+    this.testo = testo;
+  }
+
+  emails(): void{
+
+  }
+
+  oggetti(): void{
+
+  }
+
+  testi(): void{
+
+  }
+}
