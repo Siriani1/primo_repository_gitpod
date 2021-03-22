e38b4569505cf1a301c4c1574f0c70ed0ca17d26
Siriani1 <siriani.andrea@iisgalvanimi.edu.it>
2021-03-22 09:56:39 +0000

    punto otto fatto con punto sette

--- Siriani-Verifica/feisbuc-siriani/src/app/app.module.ts
+++ Siriani-Verifica/feisbuc-siriani/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { CommentComponent } from './comment/comment.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CommentComponent } from './comment/comment.component';
     CommentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
