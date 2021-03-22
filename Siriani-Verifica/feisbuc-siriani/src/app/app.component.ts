6e45a5ad73493c5c91cb7c05602c22c22b8b0558
Siriani1 <siriani.andrea@iisgalvanimi.edu.it>
2021-03-22 08:53:13 +0000

    quarto punto

--- Siriani-Verifica/feisbuc-siriani/src/app/app.component.ts
+++ Siriani-Verifica/feisbuc-siriani/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'feisbuc-siriani';
+
+  posts:Post[] = new Array<Post>();
+
+  addPost(nome: HTMLInputElement, testo: HTMLInputElement): boolean{
+    this.posts.push(new Post(nome.value,testo.value))
+    console.log(this.posts)
+    return false;
+  }
 }
