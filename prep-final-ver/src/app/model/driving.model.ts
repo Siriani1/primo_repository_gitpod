750ef79a3c9dc6db259bf6c6a642503812eaaae4
Siriani1 <siriani.andrea@iisgalvanimi.edu.it>
2021-04-12 08:27:25 +0000

    primo punto

---
+++ prep-final-ver/src/app/model/driving.model.ts
@@ -0,0 +1,13 @@
+export class Mezzo{
+
+
+  constructor(
+    public tipo: string,
+    public descrizione: string,
+    public tariffa: string,
+    public valutazioneMedia: string
+  ){
+
+  }
+
+}
