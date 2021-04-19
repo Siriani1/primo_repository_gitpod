39c598dca2a59a3cfd2be1afcf460213010cdb5d
Siriani1 <siriani.andrea@iisgalvanimi.edu.it>
2021-04-19 07:24:13 +0000

    punto due

---
+++ prep-final-ver/src/app/driving-list/driving-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Mezzo } from '../model/driving.model';
+
+@Component({
+  selector: 'app-driving-list',
+  templateUrl: './driving-list.component.html',
+  styleUrls: ['./driving-list.component.css']
+})
+export class DrivingListComponent implements OnInit {
+  @Input() listaMezzi: Mezzo[];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
